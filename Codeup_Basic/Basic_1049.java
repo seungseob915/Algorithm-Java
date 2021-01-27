70a3373d79f72e782a7435ef23c1f4c9d7324961
seungseob915 <seungseob915@naver.com>
2021-01-28 08:48:43 +0900

    2021/1/28 (add files)

---
+++ Codeup_Basic/Basic_1049.java
@@ -0,0 +1,16 @@
+package com.codeup;
+
+import java.util.Scanner;
+
+public class Basic_1049 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		int x=scan.nextInt();
+		int y=scan.nextInt();
+		
+		System.out.println(x>y? 1:0);
+	}
+
+}
