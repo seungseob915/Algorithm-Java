3a52a3f95778ab2b2c6d2fae91766e1fe81747ad
seungseob915 <seungseob915@naver.com>
2021-03-29 19:07:44 +0900

    2021/03/29 -add files

---
+++ programmers/practice/Hash_Disguise.java
@@ -0,0 +1,42 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hash_Disguise {
+	public static void main(String[] args) {
+		String[][] clothes= {{"crowmask", "face"}, 
+				{"bluesunglasses", "face"}, 
+				{"smoky_makeup", "face"}
+		};
+		
+		int ret=solution(clothes);
+		System.out.println(ret);
+	}
+	
+	public static int solution(String[][] clothes) {
+		Map<String, ArrayList<String>> cloth=new HashMap<String, ArrayList<String>>();
+        
+        for(int i=0; i<clothes.length; i++) {
+        	if(cloth.containsKey(clothes[i][1])) {
+        		ArrayList<String> temp=cloth.get(clothes[i][1]);
+        		temp.add(clothes[i][0]);
+        		cloth.put(clothes[i][1], temp);
+        	}
+        	else {
+        		ArrayList<String> temp=new ArrayList<String>();
+        		temp.add(clothes[i][0]);
+        		cloth.put(clothes[i][1], temp);
+        	}
+        }
+        
+        int cnt=1;
+        
+        for(String keys : cloth.keySet()) {
+        	cnt*=(cloth.get(keys).size()+1);
+        }
+        
+        return cnt-1;
+    }
+}
---
+++ programmers/practice/Heap_Spicy.java
@@ -0,0 +1,42 @@
+package practice;
+
+import java.util.PriorityQueue;
+
+public class Heap_Spicy {
+
+	public static void main(String[] args) {
+		int[] scoville= {1, 2, 3, 9, 10, 12};
+		int K=7;
+		
+		int ans=solution(scoville, K);
+		System.out.println(ans);
+		
+	}
+	
+	public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+        for(int i=0; i<scoville.length; i++)
+        	pq.offer(scoville[i]);
+       
+        while(true) {
+        	int now=pq.poll();
+        	
+        	if(answer>scoville.length-1) {
+        		answer=-1;
+        		break;
+        	}
+        	if(now>=K) break;
+        	
+        	int s_now=pq.poll();
+        	
+        	int new_one=now+(s_now*2);
+        	answer++;
+        	pq.offer(new_one);
+        }
+        
+        return answer;
+    }
+
+}
